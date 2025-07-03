6b99f0cbd81bb6cbf5e1958c12376b37e797a4d0
Sieng Satya <siengsatya0505@gmail.com>
2025-07-03 08:33:10 +0700

    chore: update import statements and add TeacherModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { Module } from "@nestjs/common"
+import { AppController } from "./app.controller"
+import { AppService } from "./app.service"
+import { TeacherModule } from "./teacher/teacher.module"
 
 @Module({
-  imports: [],
+  imports: [TeacherModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
