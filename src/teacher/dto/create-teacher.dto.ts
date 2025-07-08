b168ef139d5821f2979223297b44ffbf12e3ee79
Sieng Satya <siengsatya0505@gmail.com>
2025-07-08 15:25:09 +0700

    feat(teacher): add DTOs for teacher management

---
+++ src/teacher/dto/create-teacher.dto.ts
@@ -0,0 +1,52 @@
+import {
+  IsString,
+  IsOptional,
+  IsIn,
+  IsDateString,
+  IsInt
+} from "class-validator"
+
+export enum Gender {
+  MALE = "MALE",
+  FEMALE = "FEMALE"
+}
+
+export class CreateTeacherDto {
+  @IsString()
+  name: string
+
+  @IsString()
+  code: string
+
+  @IsOptional()
+  @IsIn([Gender.MALE, Gender.FEMALE])
+  gender?: Gender
+
+  @IsOptional()
+  @IsDateString()
+  dob?: string
+
+  @IsOptional()
+  @IsString()
+  subject?: string
+
+  @IsOptional()
+  @IsString()
+  classroom?: string
+
+  @IsOptional()
+  @IsString()
+  profession?: string
+
+  @IsOptional()
+  @IsString()
+  krobkan?: string
+
+  @IsOptional()
+  @IsString()
+  rank?: string
+
+  @IsOptional()
+  @IsInt()
+  userId?: number
+}
