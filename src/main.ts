1338226b66fd8ae4659e90a8de92b2db1ea4ab19
Sieng Satya <siengsatya0505@gmail.com>
2025-07-13 22:06:07 +0700

    feat: use class-validator container

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
+import { useContainer } from "class-validator"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true })
 
   // 🌐 Global Pipe applied here
   app.useGlobalPipes(
