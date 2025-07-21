8fae260d1c2ce46e45705a000da1a8860f6b3aaa
Sieng Satya <siengsatya0505@gmail.com>
2025-07-21 14:10:18 +0700

    feat: add validateUser and login function in auth service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@nestjs/common"
+import { UsersService } from "../users/users.service"
+import { JwtService } from "@nestjs/jwt"
+import * as bcrypt from "bcrypt"
+import { ConfigService } from "@nestjs/config"
+import { LoginInput, ValidateUserInput } from "./type"
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+    private configService: ConfigService
+  ) {}
+
+  async validateUser(props: ValidateUserInput) {
+    const { phone, password } = props
+    const user = await this.usersService.getByPhone(phone)
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password, ...result } = user
+      return result
+    }
+    return null
+  }
+
+  async login(props: LoginInput) {
+    const { id, phone } = props
+    const payload = { id: id, phone: phone }
+    const accessToken = this.jwtService.sign(payload)
+    const refreshToken = this.jwtService.sign(payload, {
+      secret: this.configService.get("JWT_REFRESH_SECRET"),
+      expiresIn: "7d"
+    })
+    return { accessToken, refreshToken }
+  }
+}
