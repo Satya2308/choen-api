92205ea341b7bc34a0dc3c1975396b0c7aa6c4de
Sieng Satya <siengsatya0505@gmail.com>
2025-07-21 13:43:41 +0700

    feat(auth): add auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post, Body, UnauthorizedException } from "@nestjs/common"
+import { AuthService } from "./auth.service"
+import { LoginDto } from "./dto/login.dto"
+
+@Controller("auth")
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post("login")
+  async login(@Body() dto: LoginDto) {
+    const user = await this.authService.validateUser(dto)
+    if (!user) throw new UnauthorizedException("Invalid credentials")
+    return this.authService.login(user)
+  }
+}
