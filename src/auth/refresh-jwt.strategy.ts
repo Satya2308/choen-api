d86377f8386203c9842d950f7745349de95199c3
Sieng Satya <siengsatya0505@gmail.com>
2025-07-21 14:15:17 +0700

    feat: add refresh jwt strategy for verify refresh token

---
+++ src/auth/refresh-jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@nestjs/common"
+import { PassportStrategy } from "@nestjs/passport"
+import { ExtractJwt, Strategy } from "passport-jwt"
+import { ConfigService } from "@nestjs/config"
+
+@Injectable()
+export class RefreshJwtStrategy extends PassportStrategy(
+  Strategy,
+  "jwt-refresh"
+) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get("JWT_REFRESH_SECRET")!
+    })
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub, phone: payload.phone }
+  }
+}
