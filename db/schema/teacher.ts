a26c08a1b7c1e35626b6173cfa7893732afe3f75
Sieng Satya <siengsatya0505@gmail.com>
2025-07-24 11:15:10 +0700

    .

---
+++ db/schema/teacher.ts
@@ -0,0 +1,40 @@
+import {
+  date,
+  integer,
+  pgEnum,
+  pgTable,
+  serial,
+  text,
+  timestamp
+} from "drizzle-orm/pg-core"
+import { relations } from "drizzle-orm"
+import { user } from "./user"
+
+export const gender = pgEnum("gender", ["MALE", "FEMALE"])
+
+export const teacher = pgTable("teacher", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  code: text("code").notNull().unique(),
+  gender: gender("gender"),
+  dob: date("dob"),
+  subject: text("subject"),
+  classroom: text("classroom"),
+  profession1: text("profession"),
+  profession2: text("profession1"),
+  krobkan: text("krobkan"),
+  rank: text("rank"),
+  userId: integer("userId").references(() => user.id, {
+    onDelete: "cascade",
+    onUpdate: "cascade"
+  }),
+  createdAt: timestamp("createdAt").notNull().defaultNow(),
+  updatedAt: timestamp("updatedAt").$onUpdateFn(() => new Date())
+})
+
+export const teacherRelations = relations(teacher, ({ one }) => ({
+  user: one(user, {
+    fields: [teacher.userId],
+    references: [user.id]
+  })
+}))
---
+++ db/schema/user.ts
@@ -0,0 +1,19 @@
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core"
+import { relations } from "drizzle-orm"
+import { teacher } from "./teacher"
+
+export const user = pgTable("user", {
+  id: serial("id").primaryKey(),
+  phone: text("phone").notNull().unique(),
+  password: text("password").notNull(),
+  refreshToken: text("refreshToken"),
+  createdAt: timestamp("createdAt").notNull().defaultNow(),
+  updatedAt: timestamp("updatedAt").$onUpdateFn(() => new Date())
+})
+
+export const userRelations = relations(user, ({ one }) => ({
+  teacher: one(teacher, {
+    fields: [user.id],
+    references: [teacher.userId]
+  })
+}))
