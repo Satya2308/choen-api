a68433384777b68f89bcc5ac66fe9d55892e1659
Sieng Satya <siengsatya0505@gmail.com>
2025-07-24 11:15:39 +0700

    .

--- src/db/migrations/0001_great_lady_mastermind.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "teacher" ALTER COLUMN "rank" SET DATA TYPE text;--> statement-breakpoint
-DROP TYPE "public"."rank";
--- src/db/migrations/0003_fresh_radioactive_man.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "teacher" ADD COLUMN "profession1" text;
