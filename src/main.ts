d17b1a8321f75b1f34d03c3ee05333448d05e3f7
Sieng Satya <siengsatya0505@gmail.com>
2025-07-24 11:17:23 +0700

    .

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { useContainer } from "class-validator"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.enableCors({
+    origin: "http://localhost:5173", // your Remix app
+    credentials: true
+  })
+
   useContainer(app.select(AppModule), { fallbackOnErrors: true })
 
   // 🌐 Global Pipe applied here
